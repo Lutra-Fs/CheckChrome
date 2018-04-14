4623ee6920d69fc3812d033efdec01cfbf0cc857
aiokr <aiokr@foxmail.com>
2018-04-14 12:55:23 +0800

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -5,14 +5,10 @@ echo '-------------------------------------'
 echo ''
 
 cd ./tmp
-xmllint --format stable-x86.unformat.xml > stable-x86.format.xml
-xmllint --format stable-x64.unformat.xml > stable-x64.format.xml
-xmllint --format beta-x86.unformat.xml > beta-x86.format.xml
-xmllint --format beta-x64.unformat.xml > beta-x64.format.xml
-xmllint --format dev-x86.unformat.xml > dev-x86.format.xml
-xmllint --format dev-x64.unformat.xml > dev-x64.format.xml
-xmllint --format canary-x86.unformat.xml > canary-x86.format.xml
-xmllint --format canary-x64.unformat.xml > canary-x64.format.xml
+xmllint --format stable-arm.unformat.xml > stable-arm.format.xml
+xmllint --format beta-x86.unformat.xml > beta-arm.format.xml
+xmllint --format dev-x86.unformat.xml > dev-arm.format.xml
+xmllint --format canary-x64.unformat.xml > canary-arm.format.xml
 
 echo ''
 echo '-------------------------------------'
@@ -27,53 +23,29 @@ echo '<chromechecker>' >> chrome.tmp.xml
 DATE="$(echo $(date --rfc-2822))"
 echo '<time checktime="'$DATE'"/>' >> chrome.tmp.xml
 
-echo '<stable86>' >> chrome.tmp.xml
-cat stable-x86.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat stable-x86.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat stable-x86.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</stable86>' >> chrome.tmp.xml
+echo '<stablearm>' >> chrome.tmp.xml
+cat stable-arm.format.xml | grep '<manifest version' >> chrome.tmp.xml
+cat stable-arm.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
+cat stable-arm.format.xml | grep '<package fp' >> chrome.tmp.xml
+echo '</stablearm>' >> chrome.tmp.xml
 
-echo '<stable64>' >> chrome.tmp.xml
-cat stable-x64.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat stable-x64.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat stable-x64.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</stable64>' >> chrome.tmp.xml
+echo '<betaarm>' >> chrome.tmp.xml
+cat beta-arm.format.xml | grep '<manifest version' >> chrome.tmp.xml
+cat beta-arm.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
+cat beta-arm.format.xml | grep '<package fp' >> chrome.tmp.xml
+echo '</betaarm>' >> chrome.tmp.xml
 
-echo '<beta86>' >> chrome.tmp.xml
-cat beta-x86.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat beta-x86.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat beta-x86.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</beta86>' >> chrome.tmp.xml
-
-echo '<beta64>' >> chrome.tmp.xml
-cat beta-x64.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat beta-x64.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat beta-x64.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</beta64>' >> chrome.tmp.xml
-
-echo '<dev86>' >> chrome.tmp.xml
-cat dev-x86.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat dev-x86.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
+echo '<devarm>' >> chrome.tmp.xml
+cat dev-arm.format.xml | grep '<manifest version' >> chrome.tmp.xml
+cat dev-arm.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
 cat dev-x86.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</dev86>' >> chrome.tmp.xml
+echo '</devarm>' >> chrome.tmp.xml
 
-echo '<dev64>' >> chrome.tmp.xml
-cat dev-x64.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat dev-x64.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat dev-x64.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</dev64>' >> chrome.tmp.xml
-
-echo '<canary86>' >> chrome.tmp.xml
-cat canary-x86.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat canary-x86.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat canary-x86.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</canary86>' >> chrome.tmp.xml
-
-echo '<canary64>' >> chrome.tmp.xml
-cat canary-x64.format.xml | grep '<manifest version' >> chrome.tmp.xml
-cat canary-x64.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
-cat canary-x64.format.xml | grep '<package fp' >> chrome.tmp.xml
-echo '</canary64>' >> chrome.tmp.xml
+echo '<canaryarm>' >> chrome.tmp.xml
+cat canary-arm.format.xml | grep '<manifest version' >> chrome.tmp.xml
+cat canary-arm.format.xml | grep '<url codebase="https://dl.google.com/' >> chrome.tmp.xml
+cat canary-arm.format.xml | grep '<package fp' >> chrome.tmp.xml
+echo '</canaryarm>' >> chrome.tmp.xml
 
 echo '</chromechecker>' >> chrome.tmp.xml
 
