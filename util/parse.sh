c8ddc68f4f3bb1baceca561b5013dac4226d326c
ZHANG Bo <zhangbo163126@outlook.com>
2021-04-20 12:31:27 +1000

    Fixed: wrong variable name in echo command

--- util/parse.sh
+++ util/parse.sh
@@ -27,16 +27,17 @@ do
     # sed -n '22p' ./tmp/parse/${i}.info >> ./tmp/parse/${i}-result.info
     # sed -n '24p' ./tmp/parse/${i}.info >> ./tmp/parse/${i}-result.info
     VERSION=$(xmllint --xpath "string(/response/app/updatecheck/manifest/@version)" ./tmp/checker/${i}.xml) 
-    #URL1=$(xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://redirector.gvt1.com')]/@codebase)" ./tmp/checker/${i}.xml)
-    #URL2=$(xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://dl.google.com')]/@codebase)" ./tmp/checker/${i}.xml)
+    URL1=$(xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://redirector.gvt1.com')]/@codebase)" ./tmp/checker/${i}.xml)
+    URL2=$(xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://dl.google.com')]/@codebase )" ./tmp/checker/${i}.xml)
     INSTALLER=$(xmllint --xpath "string(/response/app/updatecheck/manifest/actions/action[@event='install']/@run)" ./tmp/checker/${i}.xml)
     SHA256=$(xmllint --xpath "string(/response/app/updatecheck/manifest/packages/package/@hash_sha256)" ./tmp/checker/${i}.xml)
     SIZE=$(xmllint --xpath "string(/response/app/updatecheck/manifest/packages/package/@size)" ./tmp/checker/${i}.xml)
     echo $VERSION  > ./tmp/parse/${i}-result.info
-    #echo $URLBASE1  >> ./tmp/parse/${i}-result.info
-    xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://redirector.gvt1.com')]/@codebase)" ./tmp/checker/${i}.xml >> ./tmp/parse/${i}-result.info
-    #echo $URLBASE2 >> ./tmp/parse/${i}-result.info
-    xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://www.google.com')]/@codebase)" ./tmp/checker/${i}.xml >> ./tmp/parse/${i}-result.info
+    echo $URL1  >> ./tmp/parse/${i}-result.info
+    #xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://redirector.gvt1.com')]/@codebase)" ./tmp/checker/${i}.xml >> ./tmp/parse/${i}-result.info
+    echo $URL2 >> ./tmp/parse/${i}-result.info
+    #xmllint --xpath "string(/response/app/updatecheck/urls/url[starts-with(@codebase,'https://www.google.com')]/@codebase)" ./tmp/checker/${i}.xml >> ./tmp/parse/${i}-result.info
+    sed -i 's|https://||g' ./tmp/parse/${i}-result.info
     echo $INSTALLER  >> ./tmp/parse/${i}-result.info
     echo $SHA256  >> ./tmp/parse/${i}-result.info
     echo $SIZE  >> ./tmp/parse/${i}-result.info
