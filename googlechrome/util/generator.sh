faac094399898e6c90fcb4b6360d50c674db329f
unknown <issaclin32@gmail.com>
2021-11-26 00:40:56 +0800

    change path

--- googlechrome/util/generator.sh
+++ googlechrome/util/generator.sh
@@ -6,15 +6,15 @@ echo '-------------------------------------'
 for i in $@
 do
     cache="http://101.96.9.133/"
-    version=$(sed -n '1p' ./tmp/parse/${i}-result.info)
-    dl_base=$(sed -n '2p' ./tmp/parse/${i}-result.info)
-    dl_alt_base=$(sed -n '3p' ./tmp/parse/${i}-result.info)
-    dl_file=$(sed -n '4p' ./tmp/parse/${i}-result.info)
+    version=$(sed -n '1p' ./googlechrome/tmp/parse/${i}-result.info)
+    dl_base=$(sed -n '2p' ./googlechrome/tmp/parse/${i}-result.info)
+    dl_alt_base=$(sed -n '3p' ./googlechrome/tmp/parse/${i}-result.info)
+    dl_file=$(sed -n '4p' ./googlechrome/tmp/parse/${i}-result.info)
     dl="https://"${dl_base}${dl_file}
     dl_alt="https://"${dl_alt_base}${dl_file}
     dl_cache=${cache}${dl_alt_base}${dl_file}
-    sha256=$(sed -n '5p' ./tmp/parse/${i}-result.info)
-    size_raw=$(sed -n '6p' ./tmp/parse/${i}-result.info)
+    sha256=$(sed -n '5p' ./googlechrome/tmp/parse/${i}-result.info)
+    size_raw=$(sed -n '6p' ./googlechrome/tmp/parse/${i}-result.info)
     size=$(awk 'BEGIN{printf "%.3f",('$size_raw'/'1048576')}')" MB"
 
     sed -i "s|{{${i}-Version}}|$version|g" googlechrome/tmp/index.html
@@ -29,5 +29,5 @@ do
     sed -i "s|{{${i}-dl-main}}|$dl|g" googlechrome/tmp/chrome.xml
     sed -i "s|{{${i}-dl-alternative}}|$dl_alt|g" googlechrome/tmp/chrome.xml
     sed -i "s|{{${i}-dl-cache}}|$dl_cache|g" googlechrome/tmp/chrome.xml
-    sed -i "s|{{${i}-Size}}|$size_raw|g" googlechrome/tmp/chrome.xml
+    sed -i "s|{{${i}-Size}}|$size_raw|g" googlechrome/tmp/chrome.xmlb
 done
