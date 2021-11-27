219d8d79b12f70a73900602b273f6d9d81a6aaeb
unknown <issaclin32@gmail.com>
2021-11-27 21:11:17 +0800

    modify pages url

--- googlechrome/run.sh
+++ googlechrome/run.sh
@@ -18,7 +18,8 @@ sed -i "s|{{CheckTime}}|$DATE|g" googlechrome/tmp/chrome.xml
 ./googlechrome/util/generator.sh "./googlechrome/tmp" stable-x86 stable-x64 beta-x86 beta-x64 dev-x86 dev-x64 canary-x86 canary-x64
 
 xmllint --format googlechrome/tmp/chrome.xml > googlechrome/tmp/chrome_linted.xml
-cp -rf googlechrome/tmp/chrome_linted.xml ./pages/googlechrome/chrome.xml
-
 #xmllint --noblanks googlechrome/tmp/chrome.xml > googlechrome/tmp/chrome.min.xml
+
+mkdir ./pages/googlechrome -p
+cp -rf googlechrome/tmp/chrome_linted.xml ./pages/googlechrome/chrome.xml
 #cp -rf googlechrome/tmp/chrome.min.xml ./pages/googlechrome/chrome.min.xml
