949cf287ff067c4c61808f15e3fc27b90e00a2ed
unknown <issaclin32@gmail.com>
2021-11-27 17:57:52 +0800

    change path

--- googlechrome/run.sh
+++ googlechrome/run.sh
@@ -6,11 +6,12 @@ chmod +x ./googlechrome/util/checker.sh
 chmod +x ./googlechrome/util/parse.sh
 chmod +x ./googlechrome/util/generator.sh
 
-./googlechrome/util/checker.sh
+./googlechrome/util/checker.sh "./googlechrome/tmp"
 
 ./googlechrome/util/parse.sh stable-x86 stable-x64 beta-x86 beta-x64 dev-x86 dev-x64 canary-x86 canary-x64
 
-cp -rf ./googlechrome/src/chrome.xml ./googlechrome/tmp/chrome.xml
+cp -f ./googlechrome/src/chrome.xml ./googlechrome/tmp/chrome.xml
+cat ./googlechrome/tmp/chrome.xml
 
 DATE="$(echo $(TZ=UTC-8 date '+%Y-%m-%d %H:%M:%S'))"
 sed -i "s|{{CheckTime}}|$DATE|g" googlechrome/tmp/chrome.xml
