874b88fbfb87a0aad6870baa4adefbbd8861abc5
unknown <issaclin32@gmail.com>
2021-11-27 17:22:46 +0800

    change path

--- googlechrome/run.sh
+++ googlechrome/run.sh
@@ -1,7 +1,6 @@
 mkdir ./googlechrome/public -p
 mkdir ./googlechrome/tmp/checker -p
 mkdir ./googlechrome/tmp/parse -p
-mkdir ./googlechrome/tmp
 
 chmod +x ./googlechrome/util/checker.sh
 chmod +x ./googlechrome/util/parse.sh
@@ -11,7 +10,7 @@ chmod +x ./googlechrome/util/generator.sh
 
 ./googlechrome/util/parse.sh stable-x86 stable-x64 beta-x86 beta-x64 dev-x86 dev-x64 canary-x86 canary-x64
 
-cp -rf googlechrome/src/chrome.xml googlechrome/tmp/chrome.xml
+cp -rf ./googlechrome/src/chrome.xml ./googlechrome/tmp/chrome.xml
 
 DATE="$(echo $(TZ=UTC-8 date '+%Y-%m-%d %H:%M:%S'))"
 sed -i "s|{{CheckTime}}|$DATE|g" googlechrome/tmp/chrome.xml
@@ -19,7 +18,7 @@ sed -i "s|{{CheckTime}}|$DATE|g" googlechrome/tmp/chrome.xml
 ./googlechrome/util/generator.sh stable-x86 stable-x64 beta-x86 beta-x64 dev-x86 dev-x64 canary-x86 canary-x64
 
 xmllint --format googlechrome/tmp/chrome.xml > googlechrome/tmp/chrome.xml
-xmllint --noblanks googlechrome/tmp/chrome.xml > googlechrome/tmp/chrome.min.xml
+#xmllint --noblanks googlechrome/tmp/chrome.xml > googlechrome/tmp/chrome.min.xml
 
 cp -rf googlechrome/tmp/chrome.xml ./googlechrome/public/chrome.xml
-cp -rf googlechrome/tmp/chrome.min.xml ./googlechrome/public/chrome.min.xml
+#cp -rf googlechrome/tmp/chrome.min.xml ./googlechrome/public/chrome.min.xml
